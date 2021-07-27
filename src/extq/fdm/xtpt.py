40165817a49c348e1bd733c7221b24cdbfe4c241
Chatipat Lorpaiboon <chatipat@uchicago.edu>
2021-07-27 15:20:34 -0500

    Add FDM extended MFPT and FK formula

--- src/extq/fdm/xtpt.py
+++ src/extq/fdm/xtpt.py
@@ -3,8 +3,12 @@ import scipy.sparse
 import scipy.sparse.linalg
 
 from .tpt import backward_committor
+from .tpt import backward_feynman_kac
+from .tpt import backward_mfpt
 from .tpt import current
 from .tpt import forward_committor
+from .tpt import forward_feynman_kac
+from .tpt import forward_mfpt
 from .tpt import rate
 
 
@@ -88,6 +92,180 @@ def extended_backward_committor(
     g = np.concatenate(guess)
     qm = backward_committor(gen, pi, d, g)
     return qm.reshape(len(transitions), len(weights))
+
+
+def extended_forward_mfpt(
+    generator,
+    weights,
+    transitions,
+    in_domain,
+    guess,
+    dt=None,
+):
+    """Compute the forward mean first passage time.
+
+    Parameters
+    ----------
+    generator : (n_points, n_points) sparse matrix of float
+        Generator matrix.
+    weights : (n_points,) ndarray of float
+        Change of measure to the invariant distribution for each point.
+    transitions : (n_indices, n_indices) array-like
+        Possible transitions between indices. Each element
+        transitions[i, j] may be a scalar or a sparse matrix of shape
+        (n_points, n_points).
+    in_domain : (n_indices, n_points) ndarray of bool
+        Whether each point is in the domain.
+    guess : (n_indices, n_points) ndarray of float
+        Guess for the mean first passage time. Must obey boundary
+        conditions.
+    dt : float, optional
+        Timestep of the transitions, if time-dependent.
+
+    Returns
+    -------
+    (n_indices, n_points) ndarray of float
+        Forward mean first passage time at each point.
+
+    """
+    gen = _extended_generator(generator, transitions, dt=dt)
+    pi = np.concatenate([weights] * len(transitions))
+    d = np.concatenate(in_domain)
+    g = np.concatenate(guess)
+    mfpt = forward_mfpt(gen, pi, d, g)
+    return mfpt.reshape(len(transitions), len(weights))
+
+
+def extended_backward_mfpt(
+    generator,
+    weights,
+    transitions,
+    in_domain,
+    guess,
+    dt=None,
+):
+    """Compute the backward mean first passage time.
+
+    Parameters
+    ----------
+    generator : (n_points, n_points) sparse matrix of float
+        Generator matrix.
+    weights : (n_points,) ndarray of float
+        Change of measure to the invariant distribution for each point.
+    transitions : (n_indices, n_indices) array-like
+        Possible transitions between indices. Each element
+        transitions[i, j] may be a scalar or a sparse matrix of shape
+        (n_points, n_points).
+    in_domain : (n_indices, n_points) ndarray of bool
+        Whether each point is in the domain.
+    guess : (n_indices, n_points) ndarray of float
+        Guess for the mean first passage time. Must obey boundary
+        conditions.
+    dt : float, optional
+        Timestep of the transitions, if time-dependent.
+
+    Returns
+    -------
+    (n_indices, n_points) ndarray of float
+        Backward mean first passage time at each point.
+
+    """
+    gen = _extended_generator(generator, transitions, dt=dt)
+    pi = np.concatenate([weights] * len(transitions))
+    d = np.concatenate(in_domain)
+    g = np.concatenate(guess)
+    mfpt = backward_mfpt(gen, pi, d, g)
+    return mfpt.reshape(len(transitions), len(weights))
+
+
+def extended_forward_feynman_kac(
+    generator,
+    weights,
+    transitions,
+    in_domain,
+    function,
+    guess,
+    dt=None,
+):
+    """Solve the forward Feynman-Kac formula.
+
+    Parameters
+    ----------
+    generator : (n_points, n_points) sparse matrix of float
+        Generator matrix.
+    weights : (n_points,) ndarray of float
+        Change of measure to the invariant distribution for each point.
+    transitions : (n_indices, n_indices) array-like
+        Possible transitions between indices. Each element
+        transitions[i, j] may be a scalar or a sparse matrix of shape
+        (n_points, n_points).
+    in_domain : (n_indices, n_points) ndarray of bool
+        Whether each point is in the domain.
+    function : (n_indices, n_points) ndarray of float
+        Function to integrate. Must be zero outside of the domain.
+    guess : (n_indices, n_points) ndarray of float
+        Guess for the solution. Must obey boundary conditions.
+    dt : float, optional
+        Timestep of the transitions, if time-dependent.
+
+    Returns
+    -------
+    (n_indices, n_points) ndarray of float
+        Solution of the Feynman-Kac formula at each point.
+
+    """
+    gen = _extended_generator(generator, transitions, dt=dt)
+    pi = np.concatenate([weights] * len(transitions))
+    d = np.concatenate(in_domain)
+    f = np.concatenate(function)
+    g = np.concatenate(guess)
+    soln = forward_feynman_kac(gen, pi, d, f, g)
+    return soln.reshape(len(transitions), len(weights))
+
+
+def extended_backward_feynman_kac(
+    generator,
+    weights,
+    transitions,
+    in_domain,
+    function,
+    guess,
+    dt=None,
+):
+    """Solve the backward Feynman-Kac formula.
+
+    Parameters
+    ----------
+    generator : (n_points, n_points) sparse matrix of float
+        Generator matrix.
+    weights : (n_points,) ndarray of float
+        Change of measure to the invariant distribution for each point.
+    transitions : (n_indices, n_indices) array-like
+        Possible transitions between indices. Each element
+        transitions[i, j] may be a scalar or a sparse matrix of shape
+        (n_points, n_points).
+    in_domain : (n_indices, n_points) ndarray of bool
+        Whether each point is in the domain.
+    function : (n_indices, n_points) ndarray of float
+        Function to integrate. Must be zero outside of the domain.
+    guess : (n_indices, n_points) ndarray of float
+        Guess for the solution. Must obey boundary conditions.
+    dt : float, optional
+        Timestep of the transitions, if time-dependent.
+
+    Returns
+    -------
+    (n_indices, n_points) ndarray of float
+        Solution of the Feynman-Kac formula at each point.
+
+    """
+    gen = _extended_generator(generator, transitions, dt=dt)
+    pi = np.concatenate([weights] * len(transitions))
+    d = np.concatenate(in_domain)
+    f = np.concatenate(function)
+    g = np.concatenate(guess)
+    soln = backward_feynman_kac(gen, pi, d, f, g)
+    return soln.reshape(len(transitions), len(weights))
 
 
 def extended_rate(
