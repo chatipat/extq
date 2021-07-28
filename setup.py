ab180963ba9012932c574d532615bc4e88b7b19a
Chatipat Lorpaiboon <chatipat@uchicago.edu>
2021-07-27 21:29:56 -0500

    Update packaging

--- setup.py
+++ setup.py
@@ -1,7 +1,4 @@
 #!/usr/bin/env python
-# -*- encoding: utf-8 -*-
-from __future__ import absolute_import
-from __future__ import print_function
 
 import io
 import re
@@ -18,63 +15,58 @@ from setuptools import setup
 def read(*names, **kwargs):
     with io.open(
         join(dirname(__file__), *names),
-        encoding=kwargs.get('encoding', 'utf8')
+        encoding=kwargs.get("encoding", "utf8"),
     ) as fh:
         return fh.read()
 
 
 setup(
-    name='extq',
-    version='0.0.0',
-    license='MIT',
-    description='Extended committors.',
-    long_description='%s\n%s' % (
-        re.compile('^.. start-badges.*^.. end-badges', re.M | re.S).sub('', read('README.rst')),
-        re.sub(':[a-z]+:`~?(.*?)`', r'``\1``', read('CHANGELOG.rst'))
+    name="extq",
+    version="0.0.0",
+    license="MIT",
+    description="Extended committors.",
+    long_description="%s\n%s"
+    % (
+        re.compile("^.. start-badges.*^.. end-badges", re.M | re.S).sub(
+            "", read("README.rst")
+        ),
+        re.sub(":[a-z]+:`~?(.*?)`", r"``\1``", read("CHANGELOG.rst")),
     ),
-    author='Chatipat Lorpaiboon',
-    author_email='chatipat@uchicago.edu',
-    url='https://github.com/chatipat/extq',
-    packages=find_packages('src'),
-    package_dir={'': 'src'},
-    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+    author="Chatipat Lorpaiboon",
+    author_email="chatipat@uchicago.edu",
+    url="https://github.com/chatipat/extq",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    py_modules=[splitext(basename(path))[0] for path in glob("src/*.py")],
     include_package_data=True,
     zip_safe=False,
     classifiers=[
-        # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: Unix',
-        'Operating System :: POSIX',
-        'Operating System :: Microsoft :: Windows',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
-        # uncomment if you test on these interpreters:
-        # 'Programming Language :: Python :: Implementation :: IronPython',
-        # 'Programming Language :: Python :: Implementation :: Jython',
-        # 'Programming Language :: Python :: Implementation :: Stackless',
-        'Topic :: Utilities',
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX",
+        "Operating System :: Unix",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Topic :: Scientific/Engineering",
     ],
     project_urls={
-        'Documentation': 'https://extq.readthedocs.io/',
-        'Changelog': 'https://extq.readthedocs.io/en/latest/changelog.html',
-        'Issue Tracker': 'https://github.com/chatipat/extq/issues',
+        "Documentation": "https://extq.readthedocs.io/",
+        "Changelog": "https://extq.readthedocs.io/en/latest/changelog.html",
+        "Issue Tracker": "https://github.com/chatipat/extq/issues",
     },
     keywords=[
         # eg: 'keyword1', 'keyword2', 'keyword3',
     ],
-    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*',
+    python_requires=">=3.7",
     install_requires=[
-        # eg: 'aspectlib==1.1.1', 'six>=1.7',
+        "numba",
+        "numpy",
+        "scipy",
     ],
     extras_require={
         # eg:
