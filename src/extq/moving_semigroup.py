4f1de26192219456e0d1f2fa4471b3fe5704770f
Chatipat Lorpaiboon <chatipat@uchicago.edu>
2021-08-23 23:31:55 -0500

    Fix aliasing issue in moving_semigroup

--- src/extq/moving_semigroup.py
+++ src/extq/moving_semigroup.py
@@ -38,7 +38,6 @@ def moving_semigroup(a, k, f, *args):
 
     # temporary arrays
     acc = np.empty((k,) + a.shape[1:], dtype=a.dtype)
-    temp = np.empty(a.shape[1:], dtype=a.dtype)
 
     for n in range(out_len):
         j = n % (k + 1)
@@ -51,16 +50,16 @@ def moving_semigroup(a, k, f, *args):
                 else:
                     f(a[n + jj], acc[jj + 1], acc[jj], *args)
         elif j == 1:
-            temp = a[n + k - 1]
+            acc[j - 1] = a[n + k - 1]
         else:
-            f(temp, a[n + k - 1], temp, *args)
+            f(acc[j - 2], a[n + k - 1], acc[j - 1], *args)
 
         # combine accumulations
         if j == 0:
             a[n] = acc[j]
         elif j == k:
-            a[n] = temp
+            a[n] = acc[j - 1]
         else:
-            f(acc[j], temp, a[n], *args)
+            f(acc[j], acc[j - 1], a[n], *args)
 
     return a[:out_len]
