4c52f29db2e9e1fb3572b54181299cc6a864149a
Chatipat Lorpaiboon <chatipat@uchicago.edu>
2022-07-05 15:47:37 -0500

    Add function to make uniform weights

--- src/extq/utils.py
+++ src/extq/utils.py
@@ -2,6 +2,33 @@ import numpy as np
 import scipy.signal
 
 
+def uniform_weights(trajs, maxlag):
+    """Make uniform weights.
+
+    Parameters
+    ----------
+    trajs : list of (n_frames[i], ...) array-like
+        List of trajectories.
+    maxlag : int
+        Number of frames at the end of each trajectory that are required
+        to have zero weight. This is the maximum lag time the output
+        weights can be used with by other methods.
+
+    Returns
+    -------
+    list of (n_frames[i],) ndarray of float
+        Weight at each frame of the trajectory. The weights of the first
+        n_frames[i]-maxlag trajectories are one.
+
+    """
+    weights = []
+    for traj in trajs:
+        w = np.ones(np.shape(traj)[0])
+        w[len(w) - maxlag :] = 0.0
+        weights.append(w)
+    return weights
+
+
 def shift_weights(weights, lag, maxlag):
     """Convert weights for length maxlag short trajectories to
     right-aligned length lag short trajectories.
