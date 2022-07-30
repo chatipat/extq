f29257600a99184c86fbfb5f9a5a4d4d970209a7
Chatipat Lorpaiboon <chatipat@uchicago.edu>
2022-07-30 10:55:35 -0500

    Fix imports

--- src/extq/memory/__init__.py
+++ src/extq/memory/__init__.py
@@ -1,55 +1,53 @@
 from . import fdm
-from ._dga import (
-    ReweightMemory,
-    ReweightIntegralMemory,
-    ForwardCommittorMemory,
-    ForwardCommittorIntegralMemory,
-    MFPTMemory,
-    MFPTIntegralMemory,
-    BackwardCommittorMemory,
-    BackwardCommittorIntegralMemory,
-    MLPTMemory,
-    MLPTIntegralMemory,
-)
+from ._dga import BackwardCommittorIntegralMemory
+from ._dga import BackwardCommittorMemory
+from ._dga import ForwardCommittorIntegralMemory
+from ._dga import ForwardCommittorMemory
+from ._dga import MFPTIntegralMemory
+from ._dga import MFPTMemory
+from ._dga import MLPTIntegralMemory
+from ._dga import MLPTMemory
+from ._dga import ReweightIntegralMemory
+from ._dga import ReweightMemory
 from ._dga import backward_committor
 from ._dga import backward_committor_integral
-from ._dga import backward_committor_integral_matrix
-from ._dga import backward_committor_matrix
 from ._dga import backward_feynman_kac
 from ._dga import backward_feynman_kac_integral
-from ._dga import backward_feynman_kac_integral_matrix
-from ._dga import backward_feynman_kac_matrix
 from ._dga import backward_mfpt
 from ._dga import backward_mfpt_integral
-from ._dga import backward_mfpt_integral_matrix
-from ._dga import backward_mfpt_matrix
 from ._dga import backward_solve
 from ._dga import backward_transform
 from ._dga import forward_committor
 from ._dga import forward_committor_integral
-from ._dga import forward_committor_integral_matrix
-from ._dga import forward_committor_matrix
 from ._dga import forward_feynman_kac
 from ._dga import forward_feynman_kac_integral
-from ._dga import forward_feynman_kac_integral_matrix
-from ._dga import forward_feynman_kac_matrix
 from ._dga import forward_mfpt
 from ._dga import forward_mfpt_integral
-from ._dga import forward_mfpt_integral_matrix
-from ._dga import forward_mfpt_matrix
 from ._dga import forward_solve
 from ._dga import forward_transform
 from ._dga import integral
-from ._dga import integral_matrix
 from ._dga import integral_solve
 from ._dga import reweight
 from ._dga import reweight_integral
-from ._dga import reweight_integral_matrix
-from ._dga import reweight_matrix
 from ._dga import reweight_solve
 from ._dga import reweight_transform
 from ._dga import tpt_integral
-from ._dga import tpt_integral_matrix
+from ._matrix import backward_committor_integral_matrix
+from ._matrix import backward_committor_matrix
+from ._matrix import backward_feynman_kac_integral_matrix
+from ._matrix import backward_feynman_kac_matrix
+from ._matrix import backward_mfpt_integral_matrix
+from ._matrix import backward_mfpt_matrix
+from ._matrix import forward_committor_integral_matrix
+from ._matrix import forward_committor_matrix
+from ._matrix import forward_feynman_kac_integral_matrix
+from ._matrix import forward_feynman_kac_matrix
+from ._matrix import forward_mfpt_integral_matrix
+from ._matrix import forward_mfpt_matrix
+from ._matrix import integral_matrix
+from ._matrix import reweight_integral_matrix
+from ._matrix import reweight_matrix
+from ._matrix import tpt_integral_matrix
 from ._memory import extrapolate
 from ._memory import generator
 from ._memory import identity
