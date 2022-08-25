343de0eeba2f656057bd9b03c8b61fea528784c1
Chatipat Lorpaiboon <chatipat@uchicago.edu>
2022-08-25 04:37:51 -0500

    Add convenience functions to run trajectories

--- src/extq/examples/euler.py
+++ src/extq/examples/euler.py
@@ -27,3 +27,45 @@ def step3d(force, kT, dt, x, y, z):
     new_y = y + dt * fy + bd * np.random.normal()
     new_z = z + dt * fz + bd * np.random.normal()
     return new_x, new_y, new_z
+
+
+@nb.njit
+def run1d(force, kT, dt, n_steps, x, out=None):
+    if out is None:
+        out = np.empty((n_steps + 1, 1))
+    assert out.shape == (n_steps + 1, 1)
+    out[0, 0] = x
+    for i in range(n_steps):
+        x = step1d(force, kT, dt, x)
+        out[i + 1, 0] = x
+    return out
+
+
+@nb.njit
+def run2d(force, kT, dt, n_steps, x, y, out=None):
+    if out is None:
+        out = np.empty((n_steps + 1, 2))
+    assert out.shape == (n_steps + 1, 2)
+    out[0, 0] = x
+    out[0, 1] = y
+    for i in range(n_steps):
+        x, y = step2d(force, kT, dt, x, y)
+        out[i + 1, 0] = x
+        out[i + 1, 1] = y
+    return out
+
+
+@nb.njit
+def run3d(force, kT, dt, n_steps, x, y, z, out=None):
+    if out is None:
+        out = np.empty((n_steps + 1, 3))
+    assert out.shape == (n_steps + 1, 3)
+    out[0, 0] = x
+    out[0, 1] = y
+    out[0, 2] = z
+    for i in range(n_steps):
+        x, y, z = step3d(force, kT, dt, x, y, z)
+        out[i + 1, 0] = x
+        out[i + 1, 1] = y
+        out[i + 1, 2] = z
+    return out
