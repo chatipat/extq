571e9b2b3a39fec8c59abef9283db0211571c3de
Chatipat Lorpaiboon <chatipat@uchicago.edu>
2022-12-12 01:39:31 -0600

    Refactor DGA module

--- src/extq/dga/_dga.py
+++ src/extq/dga/_dga.py
@@ -3,7 +3,68 @@ import numpy as np
 from .. import linalg
 from ..stop import backward_stop
 from ..stop import forward_stop
-from ._utils import transform
+
+__all__ = [
+    "reweight",
+    "forward_committor",
+    "forward_mfpt",
+    "forward_feynman_kac",
+    "backward_committor",
+    "backward_mfpt",
+    "backward_feynman_kac",
+]
+
+
+def reweight(basis, lag, maxlag=None, guess=None, test_basis=None):
+    """Estimate the change of measure to the invariant distribution.
+
+    Parameters
+    ----------
+    basis : list of (n_frames[i], n_basis) ndarray or sparse matrix of float
+        Basis for estimating the change of measure.
+    lag : int
+        Lag time in unit of frames.
+    maxlag : int
+        Number of frames at the end of each trajectory that are required
+        to have zero weight. This is the maximum lag time the output
+        weights can be used with by other methods.
+    guess : list of (n_frames[i],) ndarray of float, optional
+        Guess for the change of measure. The last maxlag frames of
+        each trajectory must be zero.
+        If None, use uniform weights (except for the last lag frames).
+    test_basis : list of (n_frames[i], n_basis) ndarray of float, optional
+        Test basis against which to minimize the error. Must have the
+        same dimension as the basis used to estimate the change of
+        measure. If None, use the basis that is used to estimate the
+        change of measure.
+
+    Returns
+    -------
+    list of (n_frames[i],) ndarray
+        Estimate of the change of measure at each frame of the
+        trajectory.
+
+    """
+    if maxlag is None:
+        maxlag = lag
+    assert maxlag >= lag
+    if test_basis is None:
+        test_basis = basis
+    if guess is None:
+        guess = []
+        for x in basis:
+            w = np.ones(x.shape[0])
+            w[-maxlag:] = 0.0
+            guess.append(w)
+    a = 0.0
+    b = 0.0
+    for x, y, w in zip(test_basis, basis, guess):
+        assert np.all(w[-maxlag:] == 0.0)
+        wdx = linalg.scale_rows(w[:-lag], x[lag:] - x[:-lag])
+        a += wdx.T @ y[:-lag]
+        b -= np.ravel(wdx.sum(axis=0))
+    coeffs = linalg.solve(a, b)
+    return [w * (y @ coeffs + 1.0) for y, w in zip(basis, guess)]
 
 
 def forward_committor(basis, weights, in_domain, guess, lag, test_basis=None):
@@ -34,62 +95,15 @@ def forward_committor(basis, weights, in_domain, guess, lag, test_basis=None):
         trajectory.
 
     """
-    if test_basis is None:
-        test_basis = basis
-    a = 0.0
-    b = 0.0
-    for x, y, w, d, g in zip(test_basis, basis, weights, in_domain, guess):
-        assert np.all(w[-lag:] == 0.0)
-        iy = np.minimum(np.arange(lag, len(d)), forward_stop(d)[:-lag])
-        assert np.all(iy < len(d))
-        wx = linalg.scale_rows(w[:-lag], x[:-lag])
-        a += wx.T @ (y[iy] - y[:-lag])
-        b -= wx.T @ (g[iy] - g[:-lag])
-    coeffs = linalg.solve(a, b)
-    return transform(coeffs, basis, guess)
-
-
-def backward_committor(basis, weights, in_domain, guess, lag, test_basis=None):
-    """Estimate the backward committor using DGA.
-
-    Parameters
-    ----------
-    basis : list of (n_frames[i], n_basis) ndarray or sparse matrix of float
-        Basis for estimating the committor. Must be zero outside of the
-        domain.
-    weights : list of (n_frames[i],) ndarray of float
-        Change of measure to the invariant distribution for each frame.
-    in_domain : list of (n_frames[i],) ndarray of bool
-        Whether each frame of the trajectories is in the domain.
-    guess : list of (n_frames[i],) ndarray of float
-        Guess for the committor. Must obey boundary conditions.
-    lag : int
-        DGA lag time in units of frames.
-    test_basis : list of (n_frames[i], n_basis) ndarray or sparse matrix of float, optional
-        Test basis against which to minimize the error. Must have the
-        same dimension as the basis used to estimate the committor.
-        If None, use the basis that is used to estimate the committor.
-
-    Returns
-    -------
-    list of (n_frames[i],) ndarray
-        Estimate of the backward committor at each frame of the
-        trajectory.
-
-    """
-    if test_basis is None:
-        test_basis = basis
-    a = 0.0
-    b = 0.0
-    for x, y, w, d, g in zip(test_basis, basis, weights, in_domain, guess):
-        assert np.all(w[-lag:] == 0.0)
-        iy = np.maximum(np.arange(len(d) - lag), backward_stop(d)[lag:])
-        assert np.all(iy >= 0)
-        wx = linalg.scale_rows(w[:-lag], x[lag:])
-        a += wx.T @ (y[iy] - y[lag:])
-        b -= wx.T @ (g[iy] - g[lag:])
-    coeffs = linalg.solve(a, b)
-    return transform(coeffs, basis, guess)
+    return forward_feynman_kac(
+        basis,
+        weights,
+        in_domain,
+        np.zeros(len(weights)),
+        guess,
+        lag,
+        test_basis=test_basis,
+    )
 
 
 def forward_mfpt(basis, weights, in_domain, guess, lag, test_basis=None):
@@ -122,68 +136,15 @@ def forward_mfpt(basis, weights, in_domain, guess, lag, test_basis=None):
         the trajectory.
 
     """
-    if test_basis is None:
-        test_basis = basis
-    a = 0.0
-    b = 0.0
-    for x, y, w, d, g in zip(test_basis, basis, weights, in_domain, guess):
-        assert np.all(w[-lag:] == 0.0)
-        ix = np.arange(len(d) - lag)
-        iy = np.minimum(np.arange(lag, len(d)), forward_stop(d)[:-lag])
-        assert np.all(iy < len(d))
-        integral = iy - ix
-        wx = linalg.scale_rows(w[:-lag], x[:-lag])
-        a += wx.T @ (y[iy] - y[:-lag])
-        b -= wx.T @ (g[iy] - g[:-lag] + integral)
-    coeffs = linalg.solve(a, b)
-    return transform(coeffs, basis, guess)
-
-
-def backward_mfpt(basis, weights, in_domain, guess, lag, test_basis=None):
-    """Estimate the backward mean first passage time using DGA.
-
-    Parameters
-    ----------
-    basis : list of (n_frames[i], n_basis) ndarray or sparse matrix of float
-        Basis for estimating the mean first passage time. Must be zero
-        outside of the domain.
-    weights : list of (n_frames[i],) ndarray of float
-        Change of measure to the invariant distribution for each frame.
-    in_domain : list of (n_frames[i],) ndarray of bool
-        Whether each frame of the trajectories is in the domain.
-    guess : list of (n_frames[i],) ndarray of float
-        Guess for the mean first passage time. Must obey boundary
-        conditions.
-    lag : int
-        DGA lag time in units of frames.
-    test_basis : list of (n_frames[i], n_basis) ndarray or sparse matrix of float, optional
-        Test basis against which to minimize the error. Must have the
-        same dimension as the basis used to estimate the mean first
-        passage time. If None, use the basis that is used to estimate
-        the mean first passage time.
-
-    Returns
-    -------
-    list of (n_frames[i],) ndarray
-        Estimate of the backward mean first passage time at each frame of
-        the trajectory.
-
-    """
-    if test_basis is None:
-        test_basis = basis
-    a = 0.0
-    b = 0.0
-    for x, y, w, d, g in zip(test_basis, basis, weights, in_domain, guess):
-        assert np.all(w[-lag:] == 0.0)
-        ix = np.arange(lag, len(d))
-        iy = np.maximum(np.arange(len(d) - lag), backward_stop(d)[lag:])
-        assert np.all(iy >= 0)
-        integral = ix - iy
-        wx = linalg.scale_rows(w[:-lag], x[lag:])
-        a += wx.T @ (y[iy] - y[lag:])
-        b -= wx.T @ (g[iy] - g[lag:] + integral)
-    coeffs = linalg.solve(a, b)
-    return transform(coeffs, basis, guess)
+    return forward_feynman_kac(
+        basis,
+        weights,
+        in_domain,
+        np.ones(len(weights)),
+        guess,
+        lag,
+        test_basis=test_basis,
+    )
 
 
 def forward_feynman_kac(
@@ -228,7 +189,7 @@ def forward_feynman_kac(
     ):
         assert np.all(w[-lag:] == 0.0)
         iy = np.minimum(np.arange(lag, len(d)), forward_stop(d)[:-lag])
-        assert np.all(iy < len(d))
+        f = np.broadcast_to(f, len(d) - 1)
         intf = np.concatenate([np.zeros(1), np.cumsum(f)])
         integral = intf[iy] - intf[:-lag]
         wx = linalg.scale_rows(w[:-lag], x[:-lag])
@@ -236,6 +197,86 @@ def forward_feynman_kac(
         b -= wx.T @ (g[iy] - g[:-lag] + integral)
     coeffs = linalg.solve(a, b)
     return transform(coeffs, basis, guess)
+
+
+def backward_committor(basis, weights, in_domain, guess, lag, test_basis=None):
+    """Estimate the backward committor using DGA.
+
+    Parameters
+    ----------
+    basis : list of (n_frames[i], n_basis) ndarray or sparse matrix of float
+        Basis for estimating the committor. Must be zero outside of the
+        domain.
+    weights : list of (n_frames[i],) ndarray of float
+        Change of measure to the invariant distribution for each frame.
+    in_domain : list of (n_frames[i],) ndarray of bool
+        Whether each frame of the trajectories is in the domain.
+    guess : list of (n_frames[i],) ndarray of float
+        Guess for the committor. Must obey boundary conditions.
+    lag : int
+        DGA lag time in units of frames.
+    test_basis : list of (n_frames[i], n_basis) ndarray or sparse matrix of float, optional
+        Test basis against which to minimize the error. Must have the
+        same dimension as the basis used to estimate the committor.
+        If None, use the basis that is used to estimate the committor.
+
+    Returns
+    -------
+    list of (n_frames[i],) ndarray
+        Estimate of the backward committor at each frame of the
+        trajectory.
+
+    """
+    return backward_feynman_kac(
+        basis,
+        weights,
+        in_domain,
+        np.zeros(len(weights)),
+        guess,
+        lag,
+        test_basis=test_basis,
+    )
+
+
+def backward_mfpt(basis, weights, in_domain, guess, lag, test_basis=None):
+    """Estimate the backward mean first passage time using DGA.
+
+    Parameters
+    ----------
+    basis : list of (n_frames[i], n_basis) ndarray or sparse matrix of float
+        Basis for estimating the mean first passage time. Must be zero
+        outside of the domain.
+    weights : list of (n_frames[i],) ndarray of float
+        Change of measure to the invariant distribution for each frame.
+    in_domain : list of (n_frames[i],) ndarray of bool
+        Whether each frame of the trajectories is in the domain.
+    guess : list of (n_frames[i],) ndarray of float
+        Guess for the mean first passage time. Must obey boundary
+        conditions.
+    lag : int
+        DGA lag time in units of frames.
+    test_basis : list of (n_frames[i], n_basis) ndarray or sparse matrix of float, optional
+        Test basis against which to minimize the error. Must have the
+        same dimension as the basis used to estimate the mean first
+        passage time. If None, use the basis that is used to estimate
+        the mean first passage time.
+
+    Returns
+    -------
+    list of (n_frames[i],) ndarray
+        Estimate of the backward mean first passage time at each frame of
+        the trajectory.
+
+    """
+    return backward_feynman_kac(
+        basis,
+        weights,
+        in_domain,
+        np.ones(len(weights)),
+        guess,
+        lag,
+        test_basis=test_basis,
+    )
 
 
 def backward_feynman_kac(
@@ -280,7 +321,7 @@ def backward_feynman_kac(
     ):
         assert np.all(w[-lag:] == 0.0)
         iy = np.maximum(np.arange(len(d) - lag), backward_stop(d)[lag:])
-        assert np.all(iy >= 0)
+        f = np.broadcast_to(f, len(d) - 1)
         intf = np.concatenate([np.zeros(1), np.cumsum(f)])
         integral = intf[lag:] - intf[iy]
         wx = linalg.scale_rows(w[:-lag], x[lag:])
@@ -288,3 +329,7 @@ def backward_feynman_kac(
         b -= wx.T @ (g[iy] - g[lag:] + integral)
     coeffs = linalg.solve(a, b)
     return transform(coeffs, basis, guess)
+
+
+def transform(coeffs, basis, guess):
+    return [y @ coeffs + g for y, g in zip(basis, guess)]
