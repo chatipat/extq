2b2656e97a4210bd4f3ca29fa9da8067d923b176
Chatipat Lorpaiboon <chatipat@uchicago.edu>
2023-03-16 05:10:33 -0500

    Simplify scale_rows/scale_cols implementation

--- src/extq/linalg.py
+++ src/extq/linalg.py
@@ -28,12 +28,7 @@ def factorized(a):
 
 def scale_rows(a, b):
     if scipy.sparse.issparse(b):
-        if isinstance(b, scipy.sparse.csr_matrix):
-            return _scale_rows_csr(a, b)
-        elif isinstance(b, scipy.sparse.csc_matrix):
-            return _scale_rows_csc(a, b)
-        else:
-            return scipy.sparse.diags(a) @ b
+        return b.multiply(a[:, None])
     else:
         if np.ndim(b) >= 2:
             return a[:, None] * b
@@ -43,31 +38,6 @@ def scale_rows(a, b):
 
 def scale_cols(a, b):
     if scipy.sparse.issparse(a):
-        if isinstance(a, scipy.sparse.csr_matrix):
-            return _scale_cols_csr(a, b)
-        elif isinstance(a, scipy.sparse.csc_matrix):
-            return _scale_cols_csc(a, b)
-        else:
-            return a @ scipy.sparse.diags(b)
+        return a.multiply(b[None, :])
     else:
         return a * b
-
-
-def _scale_rows_csr(a, b):
-    data = np.repeat(a, np.diff(b.indptr)) * b.data
-    return scipy.sparse.csr_matrix((data, b.indices, b.indptr), shape=b.shape)
-
-
-def _scale_rows_csc(a, b):
-    data = a[b.indices] * b.data
-    return scipy.sparse.csc_matrix((data, b.indices, b.indptr), shape=b.shape)
-
-
-def _scale_cols_csr(a, b):
-    data = a.data * b[a.indices]
-    return scipy.sparse.csr_matrix((data, a.indices, a.indptr), shape=a.shape)
-
-
-def _scale_cols_csc(a, b):
-    data = a.data * np.repeat(b, np.diff(a.indptr))
-    return scipy.sparse.csc_matrix((data, a.indices, a.indptr), shape=a.shape)
