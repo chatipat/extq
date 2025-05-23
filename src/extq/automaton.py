ce59ecc2c4e33efa5be7708fd6ae2b1c27c53cd4
Chatipat Lorpaiboon <chatipat@uchicago.edu>
2025-05-23 00:22:34 -0500

    Make epsilon transitions explicit

--- src/extq/automaton.py
+++ src/extq/automaton.py
@@ -2,14 +2,19 @@ import numpy as np
 
 
 class Automaton:
-    def __init__(self, initial, transitions, final):
+    def __init__(self, initial, transitions, final, epsilon_transition=None):
         n, k = transitions.shape[:2]
+        if epsilon_transition is None:
+            epsilon_transition = np.zeros((k, k), dtype=transitions.dtype)
+
         assert initial.shape == final.shape == (k,)
         assert transitions.shape == (n, k, k)
+        assert epsilon_transition.shape == (k, k)
 
         self.initial = initial
         self.transitions = transitions
         self.final = final
+        self.epsilon_transition = epsilon_transition
 
     def __add__(self, other):
         return sum([self, other])
@@ -20,12 +25,17 @@ class Automaton:
             np.kron(self.initial, other.initial),
             _batched_kron(self.transitions, other.transitions),
             np.kron(self.final, other.final),
+            np.kron(self.epsilon_transition, other.epsilon_transition),
         )
 
     def __call__(self, events, axis=-1):
-        out = self.initial[None, :]
+        # out @ eps_star == out + out @ eps + out @ eps @ eps + ...
+        eps = self.epsilon_transition
+        eps_star = np.linalg.inv(np.identity(len(eps), dtype=eps.dtype) - eps)
+
+        out = self.initial[None, :] @ eps_star
         for i in np.moveaxis(events, axis, 0):
-            out = out @ self.transitions[i]
+            out = out @ self.transitions[i] @ eps_star
         out = out @ self.final[:, None]
         out = out.reshape(out.shape[:-2])
         return out
@@ -42,7 +52,6 @@ def one_event(weights):
 
 
 def zero_or_one(automaton):
-    assert automaton.initial @ automaton.final == 0  # automaton([]) == 0
     # zero([]) == 1
     # zero(seq) == 0 if len(seq) > 0
     zero = Automaton(
@@ -54,14 +63,11 @@ def zero_or_one(automaton):
 
 
 def one_or_more(automaton):
-    assert automaton.initial @ automaton.final == 0  # automaton([]) == 0
-    # epsilon transitions = automaton.final[:, None] @ automaton.initial[None, :]
-    # more than one epsilon transition == 0
     return Automaton(
         automaton.initial,
-        automaton.transitions
-        + automaton.transitions @ automaton.final[:, None] @ automaton.initial[None, :],
+        automaton.transitions,
         automaton.final,
+        automaton.epsilon_transition + np.outer(automaton.final, automaton.initial),
     )
 
 
@@ -70,7 +76,12 @@ def zero_or_more(automaton):
 
 
 def scale(automaton, scale):
-    return Automaton(automaton.initial * scale, automaton.transitions, automaton.final)
+    return Automaton(
+        automaton.initial * scale,
+        automaton.transitions,
+        automaton.final,
+        automaton.epsilon_transition,
+    )
 
 
 def sum(automatons):
@@ -79,7 +90,10 @@ def sum(automatons):
     initial = np.concatenate([a.initial for a in automatons])
     transitions = _batched_block_diag(*[a.transitions for a in automatons])
     final = np.concatenate([a.final for a in automatons])
-    return Automaton(initial, transitions, final)
+    epsilon_transition = _batched_block_diag(
+        *[a.epsilon_transition for a in automatons]
+    )
+    return Automaton(initial, transitions, final, epsilon_transition)
 
 
 def prod(automatons):
@@ -92,51 +106,48 @@ def prod(automatons):
 def concatenate(automatons):
     n = len(automatons[0].transitions)
     assert all(len(a.transitions) == n for a in automatons)
-
-    # initial = automatons[0].initial
-    # final = automatons[-1].final
-    # transitions = automatons[:].transitions + epsilon transitions
-    # epsilon transitions =
-    #   automatons[:-1].final[:, None] @ automatons[1:].initial[None, :]
-    # absorb epsilon transitions into the matrix to the left
-
-    initial = [automatons[0].initial]
-    for i in range(1, len(automatons)):
-        initial.append(initial[i - 1] @ automatons[i - 1].final * automatons[i].initial)
-    initial = np.concatenate(initial)
-
-    transitions = np.full((len(automatons), len(automatons)), None)
-    for i in range(len(automatons)):
-        transitions[i, i] = automatons[i].transitions
-        for j in range(i + 1, len(automatons)):
-            transitions[i, j] = (
-                transitions[i, j - 1]
-                @ automatons[j - 1].final[:, None]
-                @ automatons[j].initial[None, :]
-            )
-    transitions = _block(transitions)
-
+    initial = np.concatenate(
+        [automatons[0].initial] + [np.zeros_like(a.initial) for a in automatons[1:]]
+    )
+    transitions = _batched_block_diag(*[a.transitions for a in automatons])
     final = np.concatenate(
         [np.zeros_like(a.final) for a in automatons[:-1]] + [automatons[-1].final]
     )
-
-    return Automaton(initial, transitions, final)
+    epsilon_transition = np.full((len(automatons), len(automatons)), None)
+    for i in range(len(automatons)):
+        epsilon_transition[i, i] = automatons[i].epsilon_transition
+    for i in range(len(automatons) - 1):
+        epsilon_transition[i, i + 1] = np.outer(
+            automatons[i].final, automatons[i + 1].initial
+        )
+    epsilon_transition = _block(epsilon_transition)
+    return Automaton(initial, transitions, final, epsilon_transition)
 
 
 def prune(automaton):
     initial = automaton.initial
     transitions = automaton.transitions
     final = automaton.final
+    epsilon_transition = automaton.epsilon_transition
     while True:
         # remove states with all-zero rows and/or columns
-        nonzero_rows = np.any(transitions, axis=(0, 2)) | final.astype(bool)
-        nonzero_cols = np.any(transitions, axis=(0, 1)) | initial.astype(bool)
+        nonzero_rows = (
+            np.any(transitions, axis=(0, 2))
+            | np.any(epsilon_transition, axis=1)
+            | final.astype(bool)
+        )
+        nonzero_cols = (
+            np.any(transitions, axis=(0, 1))
+            | np.any(epsilon_transition, axis=0)
+            | initial.astype(bool)
+        )
         mask = nonzero_rows & nonzero_cols
         if np.all(mask):
             return Automaton(initial, transitions, final)
         initial = initial[mask]
         transitions = transitions[:, mask, :][:, :, mask]
         final = final[mask]
+        epsilon_transition = epsilon_transition[mask, :][:, mask]
 
 
 def _block(blocks):
