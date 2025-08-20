1535e5820104faf6612dd0d8c7d0c76521dcc895
Chatipat Lorpaiboon <chatipat@uchicago.edu>
2025-08-20 00:09:17 -0500

    Refactor dgamemtcl to use shared DGA statistics classes

--- src/extq/dgamemtcl.py
+++ src/extq/dgamemtcl.py
@@ -1,19 +1,17 @@
 """DGA with time-convolutionless memory estimators for statistics."""
 
-import numpy as np
-
-from . import linalg, utils
-from ._utils import (
-    backward_feynman_kac_propagate,
-    distribution_propagate,
-    forward_feynman_kac_propagate,
-    sum_windows,
-)
-from .stop import backward_stop, forward_stop
+from . import dgastat, linalg, utils
 from .utils import normalize_weights, uniform_weights
 
+__all__ = [
+    "reweight",
+    "forward_feynman_kac",
+    "backward_feynman_kac",
+    "DGAWithMemoryTCL",
+]
 
-def stationary_distribution(
+
+def reweight(
     basis,
     lag1,
     lag2,
@@ -24,40 +22,22 @@ def stationary_distribution(
     normalize=True,
 ):
     if maxlag is None:
-        maxlag = lag2
+        maxlag = max(lag1, lag2)
     if guess is None:
         guess = uniform_weights(basis, maxlag)
-
-    # solve DGA for coef
-    a, b = _stationary_distribution_matrices(
-        basis, guess, lag1, lag2, test_basis=test_basis
-    )
-    coef = -linalg.solve(a, b)
-
-    # compute solutions from coef
-    # solution1 and solution2 have the same projection
-    solution0 = _transform_distribution(coef, basis, guess)
-    if normalize:
-        solution0 = normalize_weights(solution0)
-    solution1 = _time_lagged_stationary_distribution(solution0, lag1)
-    solution2 = _time_lagged_stationary_distribution(solution0, lag2)
-
-    # project solution onto affine model
-    projcoef = _affine_projection_coef_distribution(
-        solution1, basis, guess, test_basis=test_basis
-    )
-    projcoef_ = _affine_projection_coef_distribution(
-        solution2, basis, guess, test_basis=test_basis
-    )
-    assert np.allclose(projcoef, projcoef_)
-    projection = _transform_distribution(projcoef, basis, guess)
+    stat = dgastat.StationaryDistribution(basis, guess, test_basis=test_basis)
+    algo = DGAWithMemoryTCL(lag1, lag2).fit(stat)
+    projection = algo.projection(stat)
+    solution1 = algo.solution1(stat)
+    solution2 = algo.solution2(stat)
     if normalize:
         projection = normalize_weights(projection)
-
+        solution1 = normalize_weights(solution1)
+        solution2 = normalize_weights(solution2)
     return projection, solution1, solution2
 
 
-def forecast(
+def forward_feynman_kac(
     basis,
     weights,
     in_domain,
@@ -68,39 +48,17 @@ def forecast(
     *,
     test_basis=None,
 ):
-    # solve DGA for coef
-    a, b = _forecast_matrices(
-        basis,
-        weights,
-        in_domain,
-        function,
-        guess,
-        lag1,
-        lag2,
-        test_basis=test_basis,
+    stat = dgastat.ForwardFeynmanKac(
+        basis, weights, in_domain, function, guess, test_basis=test_basis
     )
-    coef = -linalg.solve(a, b)
-
-    # compute solutions from coef
-    # solution1 and solution2 have the same projection
-    solution0 = _transform(coef, basis, guess)
-    solution1 = _time_lagged_forecast(solution0, in_domain, function, lag1)
-    solution2 = _time_lagged_forecast(solution0, in_domain, function, lag2)
-
-    # project solution onto affine model
-    projcoef = _affine_projection_coef(
-        solution1, basis, weights, guess, test_basis=test_basis
-    )
-    projcoef_ = _affine_projection_coef(
-        solution2, basis, weights, guess, test_basis=test_basis
-    )
-    assert np.allclose(projcoef, projcoef_)
-    projection = _transform(projcoef, basis, guess)
-
+    algo = DGAWithMemoryTCL(lag1, lag2).fit(stat)
+    projection = algo.projection(stat)
+    solution1 = algo.solution1(stat)
+    solution2 = algo.solution2(stat)
     return projection, solution1, solution2
 
 
-def aftcast(
+def backward_feynman_kac(
     basis,
     weights,
     in_domain,
@@ -112,242 +70,184 @@ def aftcast(
     test_basis=None,
 ):
     # shift weights because time 0 is at end of window
-    weights = utils.shift_weights(weights, lag2)
-
-    # solve DGA for coef
-    a, b = _aftcast_matrices(
-        basis,
-        weights,
-        in_domain,
-        function,
-        guess,
-        lag1,
-        lag2,
-        test_basis=test_basis,
+    weights = utils.shift_weights(weights, max(lag1, lag2))
+    stat = dgastat.BackwardFeynmanKac(
+        basis, weights, in_domain, function, guess, test_basis=test_basis
     )
-    coef = -linalg.solve(a, b)
-
-    # compute solutions from coef
-    # solution1 and solution2 have the same projection
-    solution0 = _transform(coef, basis, guess)
-    solution1 = _time_lagged_aftcast(solution0, in_domain, function, lag1)
-    solution2 = _time_lagged_aftcast(solution0, in_domain, function, lag2)
-
-    # project solution onto affine model
-    projcoef = _affine_projection_coef(
-        solution1, basis, weights, guess, test_basis=test_basis
-    )
-    projcoef_ = _affine_projection_coef(
-        solution2, basis, weights, guess, test_basis=test_basis
-    )
-    assert np.allclose(projcoef, projcoef_)
-    projection = _transform(projcoef, basis, guess)
-
+    algo = DGAWithMemoryTCL(lag1, lag2).fit(stat)
+    projection = algo.projection(stat)
+    solution1 = algo.solution1(stat)
+    solution2 = algo.solution2(stat)
     return projection, solution1, solution2
 
 
-def _stationary_distribution_matrices(
-    basis,
-    guess,
-    lag1,
-    lag2,
-    *,
-    test_basis=None,
-):
-    assert 0 <= lag1 < lag2
-    if test_basis is None:
-        test_basis = basis
-    n_basis = None
-    a = 0.0
-    b = 0.0
-    for x, y, w in zip(test_basis, basis, guess, strict=True):
-        n_frames = x.shape[0]
-        n_basis = x.shape[1] if n_basis is None else n_basis
-        assert x.shape == (n_frames, n_basis)
-        assert y.shape == (n_frames, n_basis)
-        assert w.shape == (n_frames,)
+class DGAWithMemoryTCL:
+    def __init__(self, lag1, lag2):
+        self.lag1 = lag1
+        self.lag2 = lag2
+        self.p_coef = self.s_coef = None
 
-        t0 = np.flatnonzero(w)  # start of window
-        if len(t0) == 0:
-            continue
-        t1 = t0 + lag1
-        t2 = t0 + lag2
-        assert t2[-1] < n_frames
+    def get_parameters(self):
+        """
+        Get fitted parameters.
 
-        wdx = linalg.scale_rows(w[t0], x[t2] - x[t1])
-        a += wdx.T @ y[t0]
-        b += np.ravel(wdx.sum(axis=0))
-    return a, b
+        Returns
+        -------
+        p_coef : (n_basis,) ndarray of float
+            Projection coefficients.
+        s_coef : (n_basis,) ndarray of float
+            Solution coefficients.
 
+        """
+        return self.p_coef, self.s_coef
 
-def _forecast_matrices(
-    basis,
-    weights,
-    in_domain,
-    function,
-    guess,
-    lag1,
-    lag2,
-    *,
-    test_basis=None,
-):
-    if test_basis is None:
-        test_basis = basis
-    function = _broadcast_integrand(function, guess)
+    def set_parameters(self, p_coef, s_coef):
+        """
+        Set fitted parameters.
 
-    assert 0 <= lag1 < lag2
-    n_basis = basis[0].shape[-1]
+        Parameters
+        ----------
+        p_coef : (n_basis,) ndarray of float
+            Projection coefficients.
+        s_coef : (n_basis,) ndarray of float
+            Solution coefficients.
 
-    a = 0.0
-    b = 0.0
+        Returns
+        -------
+        self
 
-    for x, y, w, d, f, g in zip(
-        test_basis, basis, weights, in_domain, function, guess, strict=True
-    ):
-        n_frames = len(w)
-        assert x.shape == y.shape == (n_frames, n_basis)
-        assert w.shape == d.shape == g.shape == (n_frames,)
-        assert f.shape == (n_frames - 1,)
+        """
+        self.p_coef = p_coef
+        self.s_coef = s_coef
+        return self
 
-        t0 = np.flatnonzero(w)
-        if len(t0) == 0:
-            continue
-        stop = forward_stop(d)[t0]
-        t1 = np.minimum(t0 + lag1, stop)
-        t2 = np.minimum(t0 + lag2, stop)
-        assert t2[-1] < n_frames
+    def fit(self, stat):
+        """
+        Fit statistic to data.
 
-        xw = linalg.scale_rows(w[t0], x[t0]).T
-        a += xw @ (y[t2] - y[t1])
-        b += xw @ (g[t2] - g[t1] + sum_windows(f, t1, t2))
+        Parameters
+        ----------
+        stat
+            DGA statistic.
 
-    return a, b
+        Returns
+        -------
+        self
 
+        """
+        a, b, c0 = self.matrices(stat)
+        p_coef, s_coef = solve(a, b, c0)
+        self.set_parameters(p_coef, s_coef)
+        return self
 
-def _aftcast_matrices(
-    basis,
-    weights,
-    in_domain,
-    function,
-    guess,
-    lag1,
-    lag2,
-    *,
-    test_basis=None,
-):
-    if test_basis is None:
-        test_basis = basis
-    function = _broadcast_integrand(function, guess)
+    def matrices(self, stat):
+        """
+        Compute DGA matrices.
 
-    assert 0 <= lag1 < lag2
-    n_basis = basis[0].shape[-1]
+        Parameters
+        ----------
+        stat
+            DGA statistic.
 
-    a = 0.0
-    b = 0.0
+        Returns
+        -------
+        a : pair of (n_basis, n_basis) {ndarray, sparse array} of float
+            DGA matrices for the homogeneous term.
+        b : pair of (n_basis,) {ndarray, sparse array} of float
+            DGA matrices for the nonhomogeneous term.
+        c0 : (n_basis, n_basis) {ndarray, sparse array} of float
+            Matrix of inner products of basis functions.
 
-    for x, y, w, d, f, g in zip(
-        test_basis, basis, weights, in_domain, function, guess, strict=True
-    ):
-        n_frames = len(w)
-        assert x.shape == y.shape == (n_frames, n_basis)
-        assert w.shape == d.shape == g.shape == (n_frames,)
-        assert f.shape == (n_frames - 1,)
+        """
+        a1, b1 = stat.matrices(self.lag1)
+        a2, b2 = stat.matrices(self.lag2)
+        c0 = stat.gram_matrix()
+        return (a1, a2), (b1, b2), c0
 
-        t0 = np.flatnonzero(w)
-        if len(t0) == 0:
-            continue
-        stop = backward_stop(d)[t0]
-        t1 = np.maximum(t0 - lag1, stop)
-        t2 = np.maximum(t0 - lag2, stop)
-        assert t2[0] >= 0
+    def projection(self, stat):
+        """
+        Returns the projected solution.
 
-        xw = linalg.scale_rows(w[t0], x[t0]).T
-        a += xw @ (y[t2] - y[t1])
-        b += xw @ (g[t2] - g[t1] + sum_windows(f, t2, t1))
+        Parameters
+        ----------
+        stat
+            DGA statistic.
 
-    return a, b
+        Returns
+        -------
+        list of (n_frames[i],) ndarray of float
+            Estimate of the projected solution.
 
+        """
+        p_coef, _ = self.get_parameters()
+        if p_coef is None:
+            raise ValueError
+        return stat.transform(p_coef)
 
-def _time_lagged_stationary_distribution(values, lag):
-    out = []
-    for u0 in values:
-        u1 = distribution_propagate(u0, lag)
-        out.append(u1)
-    return out
+    def solution1(self, stat):
+        """
+        Returns a stochastic approximation of the solution at `lag1`.
 
+        Parameters
+        ----------
+        stat
+            DGA statistic.
 
-def _time_lagged_forecast(values, in_domain, function, lag):
-    function = _broadcast_integrand(function, values)
-    out = []
-    for u0, d, f in zip(values, in_domain, function, strict=True):
-        u1 = forward_feynman_kac_propagate(u0, d, f, lag)
-        out.append(u1)
-    return out
+        Returns
+        -------
+        list of (n_frames[i],) ndarray of float
+            Estimate of the solution.
 
+        """
+        _, s_coef = self.get_parameters()
+        if s_coef is None:
+            raise ValueError
+        return stat.propagate(stat.transform(s_coef), self.lag1)
 
-def _time_lagged_aftcast(values, in_domain, function, lag):
-    function = _broadcast_integrand(function, values)
-    out = []
-    for u0, d, f in zip(values, in_domain, function, strict=True):
-        u1 = backward_feynman_kac_propagate(u0, d, f, lag)
-        out.append(u1)
-    return out
+    def solution2(self, stat):
+        """
+        Returns a stochastic approximation of the solution at `lag2`.
+
+        Parameters
+        ----------
+        stat
+            DGA statistic.
+
+        Returns
+        -------
+        list of (n_frames[i],) ndarray of float
+            Estimate of the solution.
+
+        """
+        _, s_coef = self.get_parameters()
+        if s_coef is None:
+            raise ValueError
+        return stat.propagate(stat.transform(s_coef), self.lag2)
 
 
-def _affine_projection_coef_distribution(values, basis, origin, *, test_basis=None):
-    if test_basis is None:
-        test_basis = basis
+def solve(a, b, c0):
+    """
+    Solve DGA with time-convolutionless memory.
 
-    n_basis = basis[0].shape[-1]
+    Parameters
+    ----------
+    a : pair of (n_basis, n_basis) {ndarray, sparse array} of float
+        DGA matrices for the homogeneous term.
+    b : pair of (n_basis,) {ndarray, sparse array} of float
+        DGA matrices for the nonhomogeneous term.
+    c0 : (n_basis, n_basis) ndarray of float
+        Matrix of inner products of basis functions.
 
-    a = 0.0
-    b = 0.0
+    Returns
+    -------
+    p_coef : (n_basis,) ndarray of float
+        Projection coefficients.
+    s_coef : (mem, n_basis) ndarray of float
+        Solution coefficients.
 
-    for x, y, w, u in zip(test_basis, basis, origin, values, strict=True):
-        n_frames = len(w)
-        assert x.shape == y.shape == (n_frames, n_basis)
-        assert w.shape == u.shape == (n_frames,)
-
-        a += x.T @ linalg.scale_rows(w, y)
-        b += x.T @ (u - w)
-
-    return linalg.solve(a, b)
-
-
-def _affine_projection_coef(values, basis, weights, origin, *, test_basis=None):
-    if test_basis is None:
-        test_basis = basis
-
-    n_basis = basis[0].shape[-1]
-
-    a = 0.0
-    b = 0.0
-
-    for x, y, w, g, u in zip(test_basis, basis, weights, origin, values, strict=True):
-        n_frames = len(w)
-        assert x.shape == y.shape == (n_frames, n_basis)
-        assert w.shape == g.shape == u.shape == (n_frames,)
-
-        t = np.flatnonzero(w)
-        if len(t) == 0:
-            continue
-
-        xw = linalg.scale_rows(w[t], x[t]).T
-        a += xw @ y[t]
-        b += xw @ (u[t] - g[t])
-
-    return linalg.solve(a, b)
-
-
-def _transform_distribution(coef, basis, guess):
-    return [w * (y @ coef + 1.0) for y, w in zip(basis, guess, strict=True)]
-
-
-def _transform(coef, basis, guess):
-    return [y @ coef + g for y, g in zip(basis, guess, strict=True)]
-
-
-def _broadcast_integrand(f, trajs):
-    if not np.iterable(f):
-        f = [np.broadcast_to(f, traj.shape[0] - 1) for traj in trajs]
-    return f
+    """
+    a1, a2 = a
+    b1, b2 = b
+    s_coef = -linalg.solve(a2 - a1, b2 - b1)
+    p_coef = s_coef + linalg.solve(c0, a1 @ s_coef + b1)
+    return p_coef, s_coef
