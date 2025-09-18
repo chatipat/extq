8a3ff16b386531ee87d907d7e9de31df73528479
Chatipat Lorpaiboon <chatipat@uchicago.edu>
2025-09-18 04:42:46 -0500

    Refactor soft committors to use Statistic class

--- src/extq/dgastat.py
+++ src/extq/dgastat.py
@@ -4,7 +4,9 @@ import numpy as np
 import scipy as sp
 
 from . import linalg
+from ._soft_utils import soft_backward_committor_kernel, soft_forward_committor_kernel
 from ._utils import sum_windows
+from .moving_semigroup import moving_matmul
 from .stop import backward_stop, forward_stop
 
 
@@ -273,6 +275,218 @@ class BackwardFeynmanKac(Statistic):
             # frames beyond endpoints of trajectory are nan
             ut = np.full(n, np.nan)
             ut[ix] = u[iy]
+            out.append(ut)
+        return _objarray(out, 1)
+
+
+class SoftForwardCommittor(Statistic):
+    def __init__(
+        self, basis, weights, stop_rate, boundary, guess, *, dt=1.0, test_basis=None
+    ):
+        if test_basis is None:
+            test_basis = basis
+        self.basis = basis
+        self.weights = weights
+        self.stop_rate = stop_rate
+        self.boundary = boundary
+        self.guess = guess
+        self.dt = dt
+        self.test_basis = test_basis
+
+        # continuous time equation is evaluated with discrete sampling
+        # using nearest neighbor interpolation
+
+        # currently, windows start/end at the center of the time steps
+        # a better choice is to integrate the start/end time over the
+        # time steps, but this is much more complicated to implement
+
+        kernel = []
+        for v, r in zip(stop_rate, boundary, strict=True):
+            k_half = soft_forward_committor_kernel(v, r, dt / 2)
+            k = k_half[:-1] @ k_half[1:]
+            kernel.append(k)
+        self._kernel = kernel
+
+    def matrices(self, lag):
+        assert lag > 0
+        basis = self.basis
+        weights = self.weights
+        guess = self.guess
+        test_basis = self.test_basis
+        kernel = self._kernel
+
+        n_basis = None
+        a = 0.0
+        b = 0.0
+        for x, y, w, k, g in zip(
+            test_basis, basis, weights, kernel, guess, strict=True
+        ):
+            n_frames = x.shape[0]
+            n_basis = x.shape[1] if n_basis is None else n_basis
+            assert x.shape == y.shape == (n_frames, n_basis)
+            assert w.shape == g.shape == (n_frames,)
+            assert k.shape == (n_frames - 1, 2, 2)
+
+            t0 = np.flatnonzero(w)  # initial time
+            if len(t0) == 0:
+                continue
+            t1 = t0 + lag  # final time
+            assert t1[-1] < n_frames  # all times < n_frames
+
+            k = moving_matmul(k.copy(), lag)[t0]
+            wx = linalg.scale_rows(w[t0], x[t0])
+            a += wx.T @ (linalg.scale_rows(k[:, 0, 0], y[t1]) - y[t0])
+            b += wx.T @ (k[:, 0, 0] * g[t1] - g[t0] + k[:, 0, 1])
+        assert isinstance(a, (np.ndarray, sp.sparse.sparray, sp.sparse.spmatrix))
+        assert isinstance(b, np.ndarray)
+        return a, b
+
+    def gram_matrix(self):
+        return gram_matrix(self.basis, self.weights, test_basis=self.test_basis)
+
+    def transform(self, coef):
+        out = [y @ coef + g for y, g in zip(self.basis, self.guess, strict=True)]
+        return _objarray(out, 1)
+
+    def transform_difference(self, coef):
+        out = [y @ coef for y in self.basis]
+        return _objarray(out, 1)
+
+    def propagate(self, input, lag):
+        assert lag >= 0
+        out = []
+        for u, k in zip(input, self._kernel, strict=True):
+            n = len(u)
+            assert u.shape == (n,)
+            assert k.shape == (n - 1, 2, 2)
+            k = moving_matmul(k.copy(), lag)
+            t0 = np.arange(n - lag)
+            t1 = t0 + lag
+            # frames beyond endpoints of trajectory are nan
+            ut = np.full(n, np.nan)
+            ut[t0] = k[:, 0, 0] * u[t1] + k[:, 0, 1]
+            out.append(ut)
+        return _objarray(out, 1)
+
+    def propagate_difference(self, input, lag):
+        assert lag >= 0
+        out = []
+        for u, k in zip(input, self._kernel, strict=True):
+            n = len(u)
+            assert u.shape == (n,)
+            assert k.shape == (n - 1, 2, 2)
+            k = moving_matmul(k.copy(), lag)
+            t0 = np.arange(n - lag)
+            t1 = t0 + lag
+            # frames beyond endpoints of trajectory are nan
+            ut = np.full(n, np.nan)
+            ut[t0] = k[:, 0, 0] * u[t1]
+            out.append(ut)
+        return _objarray(out, 1)
+
+
+class SoftBackwardCommittor(Statistic):
+    def __init__(
+        self, basis, weights, stop_rate, boundary, guess, *, dt=1.0, test_basis=None
+    ):
+        if test_basis is None:
+            test_basis = basis
+        self.basis = basis
+        self.weights = weights
+        self.stop_rate = stop_rate
+        self.boundary = boundary
+        self.guess = guess
+        self.dt = dt
+        self.test_basis = test_basis
+
+        # continuous time equation is evaluated with discrete sampling
+        # using nearest neighbor interpolation
+
+        # currently, windows start/end at the center of the time steps
+        # a better choice is to integrate the start/end time over the
+        # time steps, but this is much more complicated to implement
+
+        kernel = []
+        for v, r in zip(stop_rate, boundary, strict=True):
+            k_half = soft_backward_committor_kernel(v, r, dt / 2)
+            k = k_half[:-1] @ k_half[1:]
+            kernel.append(k)
+        self._kernel = kernel
+
+    def matrices(self, lag):
+        assert lag > 0
+        basis = self.basis
+        weights = self.weights
+        guess = self.guess
+        test_basis = self.test_basis
+        kernel = self._kernel
+
+        n_basis = None
+        a = 0.0
+        b = 0.0
+        for x, y, w, k, g in zip(
+            test_basis, basis, weights, kernel, guess, strict=True
+        ):
+            n_frames = x.shape[0]
+            n_basis = x.shape[1] if n_basis is None else n_basis
+            assert x.shape == y.shape == (n_frames, n_basis)
+            assert w.shape == g.shape == (n_frames,)
+            assert k.shape == (n_frames - 1, 2, 2)
+
+            t0 = np.flatnonzero(w)  # initial time
+            if len(t0) == 0:
+                continue
+            t1 = t0 - lag  # final time
+            assert t1[0] >= 0  # all times >= 0
+
+            k = moving_matmul(k.copy(), lag)[t1]
+            wx = linalg.scale_rows(w[t0], x[t0])
+            a += wx.T @ (linalg.scale_rows(k[:, 0, 0], y[t1]) - y[t0])
+            b += wx.T @ (k[:, 0, 0] * g[t1] - g[t0] + k[:, 1, 0])
+        assert isinstance(a, (np.ndarray, sp.sparse.sparray, sp.sparse.spmatrix))
+        assert isinstance(b, np.ndarray)
+        return a, b
+
+    def gram_matrix(self):
+        return gram_matrix(self.basis, self.weights, test_basis=self.test_basis)
+
+    def transform(self, coef):
+        out = [y @ coef + g for y, g in zip(self.basis, self.guess, strict=True)]
+        return _objarray(out, 1)
+
+    def transform_difference(self, coef):
+        out = [y @ coef for y in self.basis]
+        return _objarray(out, 1)
+
+    def propagate(self, input, lag):
+        assert lag >= 0
+        out = []
+        for u, k in zip(input, self._kernel, strict=True):
+            n = len(u)
+            assert u.shape == (n,)
+            assert k.shape == (n - 1, 2, 2)
+            k = moving_matmul(k.copy(), lag)
+            t0 = np.arange(lag, n)
+            t1 = t0 - lag
+            # frames beyond endpoints of trajectory are nan
+            ut = np.full(n, np.nan)
+            ut[t0] = k[:, 0, 0] * u[t1] + k[:, 1, 0]
+            out.append(ut)
+        return _objarray(out, 1)
+
+    def propagate_difference(self, input, lag):
+        assert lag >= 0
+        out = []
+        for u, k in zip(input, self._kernel, strict=True):
+            n = len(u)
+            assert u.shape == (n,)
+            assert k.shape == (n - 1, 2, 2)
+            k = moving_matmul(k.copy(), lag)
+            t0 = np.arange(lag, n)
+            t1 = t0 - lag
+            # frames beyond endpoints of trajectory are nan
+            ut = np.full(n, np.nan)
+            ut[t0] = k[:, 0, 0] * u[t1]
             out.append(ut)
         return _objarray(out, 1)
 
